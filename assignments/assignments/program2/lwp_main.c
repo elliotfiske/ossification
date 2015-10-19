dd78d0faeaf99e6b619e55bda89f66e77bf77ceb
elliotfiske <elliotfiske@gmail.com>
2015-10-19 00:02:15 -0700

    Tentatively working stuff yo

--- assignments/assignments/program2/lwp_main.c
+++ assignments/assignments/program2/lwp_main.c
@@ -13,10 +13,18 @@
 #include <stdint.h>
 
 tid_t currThreadID = 0;
-thread threadListHead; /* Keep track of all the threads so we can delete
-                           the threads on lwp_stop() */
+thread currentThread;
+thread threadListHead_sched; /* Keep track of all threads for scheduler */
+thread threadListTail_sched;
 
-void *muhStack;
+thread threadListHead_lib; /* Keep track of all the threads so we can delete */
+                           /*  the threads on lwp_stop() */
+
+//void *muhStack;
+rfile oldRFile, dummyRFile;
+scheduler currScheduler = NULL;
+
+unsigned long *oldStackPointer;
 
 rfile setupArguments(void *arguments) {
     rfile result;
@@ -32,20 +40,85 @@ rfile setupArguments(void *arguments) {
     return result;
 }
 
+
+/*******    DEFAULT SCHEDULING FUNCTIONS    ********/
+thread defaultScheduler_next() {
+    thread result = threadListHead_sched;
+    thread prevThread;
+    
+    if (result == NULL) {
+        return NULL;
+    }
+    
+    if (result->sched_one == NULL) {
+        return result;
+    }
+    
+    result->sched_one = threadListTail_sched;
+    
+    while (prevThread->sched_one != threadListHead_sched) {
+        prevThread = prevThread->sched_one;
+    }
+    
+    threadListHead_sched = prevThread;
+    prevThread->sched_one = NULL;
+    
+    return result;
+}
+
+void defaultScheduler_admit(thread newThread) {
+    if (threadListHead_sched == NULL) {
+        threadListHead_sched = newThread;
+        threadListTail_sched = newThread;
+    }
+    else {
+        newThread->sched_one = threadListTail_sched;
+        threadListTail_sched = newThread;
+    }
+}
+
+void defaultScheduler_remove(thread victim) {
+    thread prevThread;
+    
+    if (threadListTail_sched == threadListHead_sched) {
+        threadListHead_sched = threadListTail_sched = NULL; /* One left, kill it*/
+    }
+    else {
+        if (victim == threadListTail_sched) {
+            threadListTail_sched = victim->sched_one;
+        }
+        
+        prevThread = threadListTail_sched;
+        while (prevThread != NULL && prevThread->sched_one != victim) {
+            prevThread = prevThread->sched_one;
+        }
+        
+        if (prevThread == NULL) { /* Victim not found in thread list */
+            return;
+        }
+        
+        if (victim == threadListHead_sched) {
+            threadListHead_sched = prevThread;
+        }
+        
+        prevThread->sched_one = victim->sched_one;
+    }
+}
+
+
 /**
  * Spawn a sexy new thread with the specified function to run,
  *  arguments for the function, and requested stack size.
  */
 tid_t lwp_create(lwpfun functionToRun, void *arguments, size_t stackSize) {
-    thread result = malloc(sizeof(context));
+    thread result = calloc(1, sizeof(context));
     size_t stackBytes = stackSize * sizeof(unsigned long);
     
     void *threadStack = malloc(stackBytes);
     uintptr_t alignedStack = (uintptr_t) threadStack;
     alignedStack += (16 - alignedStack % 16);
-    threadStack = (void *)alignedStack;
     
-    void *threadStackBase = threadStack + stackBytes;
+    void *threadStackBase = (void *) (alignedStack + stackBytes);
     
     result->tid = currThreadID++;
     
@@ -55,6 +128,9 @@ tid_t lwp_create(lwpfun functionToRun, void *arguments, size_t stackSize) {
     result->state = setupArguments(arguments);
     
     // TODO: put something here: old base pointer?
+    *((unsigned long*) threadStackBase) = 0x11111111;
+    threadStackBase -= sizeof(unsigned long);
+    *((unsigned long*) threadStackBase) = 0x22222222;
     threadStackBase -= sizeof(unsigned long);
     
     threadStackBase -= sizeof(unsigned long);
@@ -71,18 +147,16 @@ tid_t lwp_create(lwpfun functionToRun, void *arguments, size_t stackSize) {
     
     result->state.rbp = (unsigned long) threadStackBase;
     
+//    muhStack = threadStackBase;
+    
     /* Build linked list of threads */
-    if (threadListHead == NULL) {
-        threadListHead = result;
+    if (threadListHead_lib == NULL) {
+        threadListHead_lib = result;
     }
     else {
-        threadListHead->lib_one = result;
-        result->lib_two = threadListHead;
+        result->lib_one = threadListHead_lib;
+        threadListHead_lib = result;
     }
-    
-    muhStack = threadStackBase;
-    
-    threadListHead = result;
     
     return result->tid;
 }
@@ -91,10 +165,19 @@ tid_t lwp_create(lwpfun functionToRun, void *arguments, size_t stackSize) {
  * Call this from a lwp thread to tell it to DIE
  */
 void  lwp_exit(void) {
-    int a = 10;
-    fprintf(stderr, "Called lwp_exit %d\n", a);
+    thread dummyThread; /* don't  ask */
+    dummyThread = NULL;
     
-    return;
+    SetSP(oldStackPointer);
+    
+    free(currentThread->stack);
+    if (currScheduler == NULL) {
+        defaultScheduler_remove(currentThread);
+    }
+    
+    free(currentThread);
+    
+    swap_rfiles(&dummyRFile, &oldRFile);
 }
 
 /**
@@ -115,13 +198,29 @@ void  lwp_yield(void) {
 }
 
 /**
+ * Grab the next thread from the scheduler
+ */
+thread lwp_get_next() {
+    if (currScheduler == NULL) {
+        return defaultScheduler_next();
+    }
+    
+    return currScheduler->next();
+}
+
+/**
  * Start all da threads we've lwp_create'd
  */
 void  lwp_start(void) {
-    rfile oldRfiles;
+    thread next = lwp_get_next();
     
-    swap_rfiles(&oldRfiles, &(threadListHead->state));
+    if (next != NULL) {
+        currentThread = next;
+        GetSP(oldStackPointer);
+        swap_rfiles(&oldRFile, &(threadListHead_sched->state));
+    }
 }
+
 
 /**
  * Stop all threads!
@@ -145,18 +244,28 @@ void  lwp_set_scheduler(scheduler fun) {
  *  the fate of our threads.
  */
 scheduler lwp_get_scheduler(void) {
-    scheduler result;
-    fprintf(stderr, "Called lwp_get_scheduler\n");
-    return result;
+    return currScheduler;
 }
 
 /**
- * Given a thread's ID, return that thread (or NULL) if no
+ * Given a thread's ID, return that thread (or NO_THREAD) if no
  *  such thread exists.
  */
 thread tid2thread(tid_t tid) {
-    fprintf(stderr, "Called tid2thread\n\n");
-    thread result;
+    thread result = threadListHead_lib;
+    
+    if (threadListHead_lib == NULL) {
+        return NO_THREAD;
+    }
+    
+    while (result != NULL && result->tid != tid) {
+        result = result->lib_one;
+    }
+    
+    if (result == NULL) {
+        return NO_THREAD;
+    }
+    
     return result;
 }
 
@@ -175,10 +284,11 @@ void poop(int a) {
 int main(int argc, char *argv[]) {
     int argument = 69;
     
-    lwp_create((lwpfun)poop, &argument, 100);
+    tid_t threadID = lwp_create((lwpfun)poop, &argument, 1000);
+    defaultScheduler_admit(tid2thread(threadID));
     
     lwp_start();
+    printf("I MADE IT BACK!!\n");
     
     return 0;
 }
-
